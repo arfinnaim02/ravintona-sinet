2367d1197636525de1a9afe890a030a4e970f748
Arfin Naim <arfinnaim02@gmail.com>
2026-02-15 17:06:08 +0600

    Reset migrations and clean database setup

--- ravintola_sinet/settings.py
+++ ravintola_sinet/settings.py
@@ -208,3 +208,8 @@ CLOUDINARY_STORAGE = {
 }
 
 
+if not DEBUG:
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@example.com", "admin123")
