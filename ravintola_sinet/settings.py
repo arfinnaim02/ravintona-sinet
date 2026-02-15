2f4d11460d9d02da3d4f6b3f38440f4bcd2242b6
Arfin Naim <arfinnaim02@gmail.com>
2026-02-15 17:11:03 +0600

    Removed illegal superuser creation from settings

--- ravintola_sinet/settings.py
+++ ravintola_sinet/settings.py
@@ -207,9 +207,3 @@ CLOUDINARY_STORAGE = {
     "CLOUDINARY_URL": os.environ.get("CLOUDINARY_URL", "")
 }
 
-
-if not DEBUG:
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser("admin", "admin@example.com", "admin123")
