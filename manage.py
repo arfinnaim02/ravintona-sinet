398f531e5575967598074307f4c528d4c1145559
Nazmul Arafin Naim <arfinnaim02@gmail.com>
2026-02-22 21:15:58 +0600

    Update manage.py

--- manage.py
+++ manage.py
@@ -5,12 +5,7 @@ import sys
 
 def main():
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ravintola_sinet.settings")
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and available on your PYTHONPATH?"
-        ) from exc
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
 
