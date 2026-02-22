ae0006dc942df0a9b32a4a66519574e3ec270ef8
Nazmul Arafin Naim <arfinnaim02@gmail.com>
2026-02-22 21:21:36 +0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install -r requirements.prod.txt
 
 python manage.py check
 python manage.py collectstatic --noinput
